6f0a73d68f6f84c08c6bcf137a7c3bd284942cb1
slade1930 <157079755+slade1930@users.noreply.github.com>
2026-06-30 17:44:16 -0500

    Create cloudinary_config.py

---
+++ backend/app/core/cloudinary_config.py
@@ -0,0 +1,13 @@
+# app/core/cloudinary_config.py
+import cloudinary
+from app.core.config import settings
+
+
+def init_cloudinary():
+    """Inicializa la configuración de Cloudinary"""
+    cloudinary.config(
+        cloud_name=settings.CLOUDINARY_CLOUD_NAME,
+        api_key=settings.CLOUDINARY_API_KEY,
+        api_secret=settings.CLOUDINARY_API_SECRET,
+        secure=True,
+    )
